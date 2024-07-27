ce5db8b4bb4015fa2171ab67b8e73bd2c932fbfa
Nico Elbers <nico.b.elbers@gmail.com>
2024-07-27 23:07:42 +0200

    nixify

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "A basic flake with a shell";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.zig.url = "github:mitchellh/zig-overlay";
+
+  outputs = { nixpkgs, flake-utils, zig, ... }:
+  flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.default = with pkgs; mkShell {
+        packages = [ 
+          bashInteractive 
+          zig.packages.${system}.master
+        ];
+      };
+    });
+}
